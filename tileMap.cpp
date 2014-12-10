1dfb5c20eb163faf96ae62e16f304ed6dd149738
kaitokidi <genis.bayona@est.fib.upc.edu>
2014-12-10 19:34:26 +0100

    Making it work and better built

--- tileMap.cpp
+++ tileMap.cpp
@@ -1,5 +1,4 @@
 /************************************TILEMAP********************************/
-
 #include<SFML/Graphics.hpp>
 #include<iostream>
 #include<fstream>
@@ -8,50 +7,61 @@
 
 int main(){
 
+    //WITH THIS WE ASSOCIATE THE FILE Map.txt TO mapFile
     std::ifstream mapFile("Map.txt");
 
-    sf::Texture tileTexture;
+    sf::Texture tileMapTexture;
     sf::Sprite tile;
 
-    sf::Vector2i map[100][100];
-    sf::Vector2i source;
-    source.x = 0;
-    source.y = 0;
+    std::vector < std::vector < sf::Vector2i> > map;
+    sf::Vector2i source(0,0);
 
     std::string str;
     int spritew, spriteh;
-    int width, heigth;
+    int sourceTailWidth, sourceTailHeigh;
+    int qttyCol, qttyRow;
 
+    //WITH THIS WE OPPEN THE FILE AND IF SUCCEEDS CONTINUE
     if(mapFile.is_open()){
 
-        std::string tileset;
-        mapFile >> tileset;
-        if(!tileTexture.loadFromFile(tileset)){
+        std::string tileMapImageName;
+        //THE OPERATOR   ">>"   MEANS PUT THE NEXT STRING ON THE mapFile ON THE STRING NEXT TO IT (in  this case tileMapImageName)
+        mapFile >> tileMapImageName;
+        if(!tileMapTexture.loadFromFile(tileMapImageName)){
             std::cout << "failed on loading tileset" << std::endl;
         }
-        tile.setTexture(tileTexture);
+        tile.setTexture(tileMapTexture);
 
         //Sprite size
         mapFile >> str;
-        spritew = atoi(str.c_str());
+        //.c_str() converts a C++ string to a C string
+        //I did it to aply atoi on it which is a C function that converts strings to integers
+        sourceTailWidth = atoi(str.c_str());
         mapFile >> str;
-        spriteh = atoi(str.c_str());
+        sourceTailHeigh = atoi(str.c_str());
 
+        //ARBITRARY VALUES (the values we want our tiles to have on the screen)
+	    spritew = 64;	    spriteh = 64;
+	
         //Map size
+        //WITH THIS IMPLEMENTATION YOU CAN ONLY USE MAPS WITH A MAXIMUM SIZE OF 9X9
+        //a better implementation would take the first characters of the string str before the ','
+        //and the characters after it and use the function .c_str() and atoi like wi did  above
         mapFile >> str;
-        width = str[0] - '0';
+        qttyCol = str[0] - '0';
         mapFile >> str;
-        heigth = str[0] - '0';
+        qttyRow = str[0] - '0';
+                
+        map = std::vector < std::vector < sf::Vector2i> > (qttyRow, std::vector < sf::Vector2i> (qttyCol));
 
         while(!mapFile.eof()){
             mapFile >> str;
-//              std::cout << " -> " << str << std::endl;
             char x = str[0]; char y = str[2];
             if(x == 'n' || y == 'n')
+            //THERE IS NO CONTROL AT PAINTING IT SO IMPLEMENT THE CONTROL OR DO NOT DO THIS : D
                 map[source.x][source.y] = sf::Vector2i(-1,-1);
             else{
                 map[source.x][source.y] = sf::Vector2i(x - '0', y - '0');
-//                std::cout << source.x << " " << source.y << " -> " << x - '0' << "." << y - '0' << std::endl;
             }
 
             if(mapFile.peek() == '\n'){
@@ -62,8 +72,11 @@ int main(){
                 ++source.x;
         }
     }
-   std::cout << width*spritew << std::endl;
-    sf::RenderWindow window(sf::VideoMode(width*spritew+1,heigth*spriteh+1), "title");
+    else {
+        std::cout << "Can't Open the file" << std::endl;
+    }
+    
+    sf::RenderWindow window(sf::VideoMode(qttyCol*spritew,qttyRow*spriteh), "title");
 
     while(window.isOpen()){
         sf::Event event;
@@ -76,19 +89,17 @@ int main(){
                     break;
             }
         }
-
         window.clear(sf::Color(250,250,250));
-        for(int i = 0; i < width; ++i){
-            for(int j = 0; j < heigth; ++j){
+	    tile.scale(2,2);
+        for(int i = 0; i < qttyCol; ++i){
+            for(int j = 0; j < qttyRow; ++j){
                 tile.setPosition(i*spritew, j*spriteh);
-                tile.setTextureRect(sf::IntRect(map[i][j].x*spritew,
-                    map[i][j].y*spriteh,spritew,spriteh));
-
+                tile.setTextureRect(sf::IntRect(map[i][j].x*sourceTailWidth, map[i][j].y*sourceTailHeigh,sourceTailWidth,sourceTailHeigh));
+		        tile.setScale(spritew/sourceTailWidth,spriteh/sourceTailHeigh);
                 window.draw(tile);
             }
         }
-
-        window.display();
+        window.display();	
     }
 
 }
@@ -96,5 +107,5 @@ int main(){
 /*
 g++ -c tileMap.cpp
 g++ -o tilemap tileMap.o -lsfml-graphics -lsfml-window -lsfml-system
+./tilemap
  */
-
